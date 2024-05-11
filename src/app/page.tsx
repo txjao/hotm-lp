4f7afcdb441b30f7d102e5d8d42ae05e61e66c25
Joao Teixeira <jaobacanao@gmail.com>
2024-05-11 19:15:29 -0300

    feat: add desktop and lg mobile dispositives responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
       <div className="first-section">
         <header className="fixed flex h-[10vh] w-full justify-end items-center p-10 bg-black">
           <div className="flex gap-20 text-">
-            <a href="#second-section" className="text-yellow text-lg underline"> Saiba Mais </a>
-            <a href="" className="text-yellow text-lg underline"> Quem sou </a>
-            <a href="" className="text-yellow text-lg underline"> Adquira já </a>
+            <a href="#third-section" className="text-yellow text-lg underline hover:relative hover:bottom-px md:text-base"> Saiba Mais </a>
+            <a href="#second-section" className="text-yellow text-lg underline hover:relative hover:bottom-px md:text-base"> Quem sou </a>
+            <a href="https://pay.hotmart.com/E92520462X" className="text-yellow text-lg underline hover:relative hover:bottom-px md:text-base"> Adquira já </a>
           </div>
         </header>
         <section id="banner" className={`flex justify-center items-center bg-[url('../assets/bannerHero.png')] bg-no-repeat bg-cover bg-center h-[99.8vh]`}>
           <div className="flex items-center flex-col gap-16">
             <div className="flex items-center flex-col">
-              <h1 className="text-xl font-bold text-yellow w-[60%] text-center">JÁ IMAGINOU APRENDER A CONQUISTAR LINDAS MULHERES UTILIZANDO SUA REDE SOCIAL?</h1>
+              <h1 className="text-xl font-bold text-yellow w-[60%] text-center md:w-[80%]">JÁ IMAGINOU APRENDER A CONQUISTAR LINDAS MULHERES UTILIZANDO SUA REDE SOCIAL?</h1>
               <p className="text-lg w-[50%] text-center ">Aprender a conquistar mulheres no Instagram de forma cuidadosa, respeitosa e eficaz. Desenvolva métodos de abordagem que garatem sucesso</p>
             </div>
-            <a href="" className="text-black font-bold bg-green h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-green-hover"> Comece Agora!</a>
+            <a href="https://pay.hotmart.com/E92520462X" className="text-black font-bold bg-green h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-green-hover"> Comece Agora!</a>
           </div>
         </section>
         <div className="bg-red w-full h-[2px]"></div>
@@ -50,21 +50,21 @@ export default function Home() {
         <div className="flex items-center flex-col w-3/4 gap-20">
           <p className="text-black text-base text-left">
             Isso é o tipo de abordagem que você irá aprender, você terá um repertório de abordagem e de assuntos que irão te destacar em meio à multidão, você se tornará mais desejado com a minha metodologia.<br />Não perca essa oportunidade? Conseguir falar com as mulheres que sempre viu e desejou, e faze-las te querer? Tudo isso com o <strong className="text-red font-semibold">sigilo garantido</strong>, clique em adquira já e se torne aquilo que sempre sonhou ser!</p>
-          <a href="" className="text-black font-bold bg-green h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-green-hover"> Comece Agora!</a>
+          <a href="https://pay.hotmart.com/E92520462X" className="text-black font-bold bg-green h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-green-hover"> Comece Agora!</a>
         </div>
       </section>
       <section id="fourth-section" className="flex h-[50vh] bg-gradient-to-r from-black via-black to-red justify-between">
         <Image alt="casal" src={casalBar} className="w-[40%]" />
         <div className="flex justify-center items-center flex-col gap-20 text-center">
           <p className="text-base w-[80%]">Aprenda a conquistar mulheres nas redes sociais de forma cuidadosa, respeitosa e eficaz. Desenvolva métodos de abordagem que garantem sucesso!<br /><strong className="text-red-secondary font-semibold">Comece já!</strong></p>
-          <a href="" className="text-black font-bold bg-yellow h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-yellow-hover"> Comece Agora!</a>
+          <a href="https://pay.hotmart.com/E92520462X" className="text-black font-bold bg-yellow h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-yellow-hover"> Comece Agora!</a>
         </div>
       </section>
       <section id="fifth-section" className="flex bg-white h-[80vh] justify-between items-center p-16 gap-20">
         <div className="flex items-center flex-col w-3/4 gap-20">
           <p className="text-black text-base text-left">
             Isso é o tipo de abordagem que você irá aprender, você terá um repertório de abordagem e de assuntos que irão te destacar em meio à multidão, você se tornará mais desejado com a minha metodologia.<br />Não perca essa oportunidade? Conseguir falar com as mulheres que sempre viu e desejou, e faze-las te querer? Tudo isso com o <strong className="text-red font-semibold">sigilo garantido</strong>, clique em adquira já e se torne aquilo que sempre sonhou ser!</p>
-          <a href="" className="text-black font-bold bg-yellow h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-yellow-hover text-center"> Comece Agora <br /> Mesmo!</a>
+          <a href="https://pay.hotmart.com/E92520462X" className="text-black font-bold bg-yellow h-16 w-60 flex justify-center items-center rounded-full transition-colors duration-200 hover:bg-yellow-hover text-center"> Comece Agora <br /> Mesmo!</a>
         </div>
         <div className="">
           <Image alt="casal" src={prints1} className="h-[400px]" />
