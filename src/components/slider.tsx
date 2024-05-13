19ffa78dc953e02e05d9d91ff98ce51bac02b1bd
João <jaobacanao@gmail.com>
2024-05-13 10:46:52 -0300

    fix: fix img proportion

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export function Slider() {
     return (
         <Carousel showArrows={false} className="h-[40%]">
-            <Image alt="print" src={prints.print1} className="h-10 w-10" />
+            <Image alt="print" src={prints.print1} className="" />
             <Image alt="print" src={prints.print2} className="" />
           </Carousel>
     )
