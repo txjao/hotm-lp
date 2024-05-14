42841d0d37e4de31693396f5e8cac18d6342db2c
João <jaobacanao@gmail.com>
2024-05-14 12:30:23 -0300

    hotfix: add correct origin on iframe URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         <div className="flex flex-colgap-4 bg-gradient-to-b from-black via-black to-red md:justify-between md:h-[50vh] md:bg-gradient-to-r md:flex-row">
           <iframe
             width="100%" height="auto"
-            src="http://www.youtube.com/embed//8_48FQ3VFHM?si=zhA-8tGkGRFW25Hk?enablejsapi=1&origin=http://example.com">
+            src="http://www.youtube.com/embed//8_48FQ3VFHM?si=zhA-8tGkGRFW25Hk?enablejsapi=1&origin=https://hotm-lp.vercel.app/">
           </iframe>
           <div className="flex justify-center items-center text-justify">
             <p className="text-sm w-[80%] xl:text-baseSm lg:text-sm md:text-xs">Já imaginou aprender conquistar lindas mulheres utilizando sua rede social? Isso mesmo, com o meu método e estratégia de abordagem, será possível intrigar e chamar a atenção daquela mulher que você somente olha e não tem coragem de chegar, achando que não tem chance e é incapaz, não caia na enganação desses métodos por ai que ensina através de cantadas ou mensagens prontas, isso não funciona, o que de fato funciona é uma boa abordagem, utilizando as palavras certas no momento certo, adquira já nosso conteúdo e comece hoje mesmo, e tem mais, Comigo você terá o <strong className="text-yellow font-semibold">sigilo garantido</strong> e um grupo privado no telegram, para dicas diárias e lives constantes para aprender ainda mais, o que esta esperando?</p>
