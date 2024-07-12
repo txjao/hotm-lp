df148316829b1a58c1458fd24cd6db21c4f694e9
joaoteixeira <jaobacanao@gmail.com>
2024-07-12 18:35:04 -0300

    fix: add new component

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,16 +1,30 @@
-
 interface cardProps {
     children: React.ReactNode;
     type: 'red' | 'gray';
     title: string;
+    hover?: boolean;
 }
 
 export default function Card(props: cardProps) {
 
+   const handleColor = () => {
+        if(props.type === 'red') {
+            return 'bg-red text-white';
+        } else {
+            return 'bg-gray text-black';
+        }
+   }
+
+   const handleHover = () => {
+         if(props.hover) {
+              return 'hover:bg-opacity-95 hover:transform hover:scale-[1.01]';
+         }
+   }
+
     return(
-        <div className="">
-            <strong>props.title</strong>
-            {props.children}
+        <div className={`${handleColor()} ${handleHover()} flex flex-col h-60 w-48 rounded justify-center items-center transition-all duration-75`}>
+            <strong>{props.title}</strong>
+            <p className="text-sm text-center mx-2">{props.children}</p>
         </div>
     )
 }
