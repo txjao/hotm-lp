2093c25e7ff3e86c2667b9d96cfdc5d3cd1c01c3
joaoteixeira <jaobacanao@gmail.com>
2024-09-11 21:49:27 -0300

    fix: fixes deploy

--- src/app/components/PixelEvents.tsx
+++ src/app/components/PixelEvents.tsx
@@ -1,17 +1,19 @@
 "use client";
 import React, { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-
+import { env } from "process";
 
 export const FacebookPixelEvents: React.FC = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1198889008013817";
+
 
   useEffect(() => {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init(process.env.NEXT_PUBLIC_META_PIXEL_ID as string);
+        ReactPixel.init(pixelId);
         ReactPixel.pageView();
       });
   }, [pathname, searchParams]);
