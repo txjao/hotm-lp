0dff27aa9e5ed4c846043f6636a3aebc07e731d4
joaoteixeira <jaobacanao@gmail.com>
2024-09-11 22:02:27 -0300

    fix: remove static env value

--- src/app/components/PixelEvents.tsx
+++ src/app/components/PixelEvents.tsx
@@ -6,14 +6,12 @@ import { env } from "process";
 export const FacebookPixelEvents: React.FC = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1198889008013817";
-
 
   useEffect(() => {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init(pixelId);
+        ReactPixel.init(process.env.NEXT_PUBLIC_META_PIXEL_ID as string);
         ReactPixel.pageView();
       });
   }, [pathname, searchParams]);
